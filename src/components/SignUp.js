5184f61356c1d68fa1d8fdb75730eaac1d569e7d
Nana Adjei Manu <claeusdev@gmail.com>
2019-02-16 03:53:34 +0000

    add email/password sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-
+import { auth } from '../firebase';
 class SignUp extends Component {
   state = { displayName: '', email: '', password: '' };
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { name, value } = event.target;
 
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { email, password, displayName } = this.state;
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      user.updateProfile({ displayName });
+    } catch (error) {
+      console.error(error);
+    }
 
     this.setState({ displayName: '', email: '', password: '' });
   };
